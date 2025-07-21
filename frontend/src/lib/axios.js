d6ab99c08c89091f738e6299c9c933d319945296
yashmolawade <yashmolawade06@gmail.com>
2025-07-21 20:26:08 +0530

    backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api",
+  baseURL: "https://letschat-backend-4jfe.onrender.com",
   withCredentials: true,
 });
